d3506c40d4183f0822e50adb6079114a24961250
songyaheng <songyaheng@hys-inc.cn>
2018-04-07 14:08:38 +0800

    dockerfile

--- server1.py
+++ server1.py
@@ -2,9 +2,9 @@ import sys
 import tensorflow as tf
 
 try:
-    worker1 = "10.244.2.131:8888"
-    worker2 = "10.244.1.126:8888"
-    worker3 = "10.244.2.132:8888"
+    worker1 = "10.244.2.140:2222"
+    worker2 = "10.244.1.134:2222"
+    worker3 = "10.244.2.141:2222"
     worker_hosts = [worker1, worker2, worker3]
     cluster_spec = tf.train.ClusterSpec({"worker": worker_hosts})
     server = tf.train.Server(cluster_spec, job_name="worker", task_index=0)
